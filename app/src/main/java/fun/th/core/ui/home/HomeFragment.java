735ba5f44a33bee9eff4f807277f181753c1ec5f
Victor Bautista <vbldoc@gmail.com>
2020-03-31 20:01:27 -0700

    initial commit (#1)

---
+++ app/src/main/java/fun/th/core/ui/home/HomeFragment.java
@@ -0,0 +1,47 @@
+package fun.th.core.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import com.th.core.R;
+import fun.th.core.TomatoActivity;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        final Button btn = root.findViewById(R.id.button);
+        btn.setOnClickListener(gameListener);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    private View.OnClickListener gameListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent myIntent = new Intent(getActivity(), TomatoActivity.class);
+            HomeFragment.this.startActivity(myIntent);
+        }
+    };
+
+}
